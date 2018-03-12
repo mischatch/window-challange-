8ba6bc390991f4dca62c4d72e64b1ef16a64ccfb
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2018-03-12 13:20:18 -0400

    added some description

--- window_challange.js
+++ window_challange.js
@@ -21,24 +21,26 @@ let inputArray = file.trim().split("\n");
 let arr1 = inputArray[0].split(' ').map(el => parseInt(el));
 let arr2 = inputArray[1].split(' ').map(el => parseInt(el));
 
-// let arr1 = [5, 3];
-// let arr2 = [188930, 194123, 201345, 154243, 154243,];
+
 
 console.log(arr1, arr2);
-// Input
-// 5 3
-// 188930 194123 201345 154243 154243
-// arr 1 - array of days and window to look
-// arr 2 - days
+// arr1 = [5, 3]; arr1 === [N, K]
+// arr2 = [188930, 194123, 201345, 154243, 154243]; arr2.length === N
 
-// find sum of numbers fo increasing and decreasing ranges in a window
+// Solution in steps:
+// — Iterate through N days of average home sale price data(arr 2) with step 1, to consider all possible ranges within a window
+// — Send each selected range to checkRange(subArr). Here we're checking which range we're dealing with, . There are two helper functions -
+// increase and decrease to check range to the end and depending on a function to return back to checkRange either 1, -1 or 0.
+// If first two element of array in checkRange are the same, and we cannot determine wheather range is encreasing or decreasing,
+// we're slicing the array and make a recursive call to the same function.
+
 
 
 function inDec(arr1, arr2){
   let n = arr1[0];
   let window = arr1[1];
   let i = 0;
-  while (i < n - window){
+  while (i < n - window + 1){
     let subArr = arr2.slice(i, window + i); // subranges with a size of a window
     console.log(checkRange(subArr)); // checking array
     i++;
@@ -51,13 +53,13 @@ function checkRange(arr){
   let i = 0;
   while (i <= arr.length - 2 ) {
     for (var j = 2; j <= arr.length; j++) {
-      let sub = arr.slice(i, j);
+      let sub = arr.slice(i, j); // creating sub ranges
       if(sub[0] < sub[1]){ // checking if the whole rnage increasing
         res += increase(sub);
       } else if(sub[0] > sub[1]){ // checking if the whole rnage decreasing
         res += decrease(sub);
       } else {
-        checkRange(sub.slice(1));
+        checkRange(sub.slice(1)); // elements are equal
       }
     }
     i++;
@@ -67,7 +69,7 @@ function checkRange(arr){
 
 function increase(arr){
   for (var i = 0; i < arr.length - 1; i++) {
-    if(arr[i] > arr[i + 1] || arr[i] === arr[i + 1]){
+    if(arr[i] > arr[i + 1] || arr[i] === arr[i + 1]){ // if range is not consecutive - return 0
       return 0;
     }
   }
@@ -76,11 +78,11 @@ function increase(arr){
 
 function decrease(arr){
   for (var i = 0; i < arr.length - 1; i++) {
-    if(arr[i] < arr[i + 1] || arr[i] === arr[i + 1]){
+    if(arr[i] < arr[i + 1] || arr[i] === arr[i + 1]){ // if range is not consecutive - return 0
       return 0;
     }
   }
   return -1;
 }
 
-// inDec(arr1, arr2);
+inDec(arr1, arr2);
